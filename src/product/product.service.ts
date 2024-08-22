e1223cd3eee8eae0e5f5d9251b7daa67b22e0949
ishumsk <ivan.shumski@ibm.com>
2024-08-22 10:31:34 +0300

    reinit project

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Product } from '@prisma/client';
+// import { Product } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -11,100 +11,43 @@ export class ProductService {
   //     data: productData,
   //   });
   // }
-
-  async getProducts(
-    page: number,
-    pageSize: number,
-    masterCategory?: string,
-    subCategory?: string,
-  ): Promise<Product[]> {
-    const skip = (page - 1) * pageSize;
-    const where = {};
-    if (masterCategory) {
-      where['masterCategory'] = masterCategory;
-    }
-    if (subCategory) {
-      where['subCategory'] = subCategory;
-    }
-
-    console.log('where', where);
-
-    return this.prisma.product.findMany({
-      where,
-      skip,
-      take: pageSize,
-      orderBy: { id: 'asc' },
-    });
-  }
-
-  public async findAllProducts(): Promise<Product[]> {
-    return this.prisma.product.findMany();
-  }
-
-  public async findProduct(productId: string): Promise<Product> {
-    return this.prisma.product.findFirst({
-      where: {
-        id: {
-          equals: productId,
-        },
-      },
-    });
-  }
   //
-  // public async getProductGender(): Promise<string[]> {
-  //   return this.getDistinctValues('gender');
-  // }
+  // async getProducts(
+  //   page: number,
+  //   pageSize: number,
+  //   masterCategory?: string,
+  //   subCategory?: string,
+  // ): Promise<Product[]> {
+  //   const skip = (page - 1) * pageSize;
+  //   const where = {};
+  //   if (masterCategory) {
+  //     where['masterCategory'] = masterCategory;
+  //   }
+  //   if (subCategory) {
+  //     where['subCategory'] = subCategory;
+  //   }
   //
-  // public async getProductMasterCategory(): Promise<string[]> {
-  //   return this.getDistinctValues('masterCategory');
-  // }
+  //   console.log('where', where);
   //
-  // public async getProductSubCategory(): Promise<string[]> {
-  //   return this.getDistinctValues('subCategory');
-  // }
-  //
-  // public async getProductArticleType(): Promise<string[]> {
-  //   return this.getDistinctValues('articleType');
-  // }
-  //
-  // public async getProductBaseColour(): Promise<string[]> {
-  //   return this.getDistinctValues('baseColour');
-  // }
-  //
-  // public async getProductSeason(): Promise<string[]> {
-  //   return this.getDistinctValues('season');
-  // }
-  //
-  // public async getProductYear(): Promise<string[]> {
-  //   return this.getDistinctValues('year');
-  // }
-  //
-  // public async getProductUsage(): Promise<string[]> {
-  //   return this.getDistinctValues('usage');
-  // }
-  //
-  // private async getDistinctValues(
-  //   columnName: keyof Product,
-  // ): Promise<string[]> {
-  //   const values = await this.prisma.product.findMany({
-  //     distinct: [columnName],
-  //     select: { [columnName]: true },
+  //   return this.prisma.product.findMany({
+  //     where,
+  //     skip,
+  //     take: pageSize,
+  //     orderBy: { id: 'asc' },
   //   });
-  //   return values.map((category) => category[columnName]);
   // }
-  // // async getDistinctValues(columnNames: (keyof Product)[]): Promise<Record<string, string[]>> {
-  // //   const distinctValues: Record<string, string[]> = {};
-  // //
-  // //   const distinctValuesResult = await this.prisma.product.groupBy({
-  // //     by: columnNames.map(columnName => ({ [columnName]: columnName })),
-  // //     orderBy: columnNames.map(columnName => ({ [columnName]: 'asc' })),
-  // //     select: columnNames.reduce((acc, columnName) => ({ ...acc, [columnName]: { distinct: true } }), {}),
-  // //   });
-  // //
-  // //   columnNames.forEach(columnName => {
-  // //     distinctValues[columnName] = distinctValuesResult.map(item => item[columnName]);
-  // //   });
-  // //
-  // //   return distinctValues;
-  // // }
+  //
+  // public async findAllProducts(): Promise<Product[]> {
+  //   return this.prisma.product.findMany();
+  // }
+  //
+  // public async findProduct(productId: string): Promise<Product> {
+  //   return this.prisma.product.findFirst({
+  //     where: {
+  //       id: {
+  //         equals: productId,
+  //       },
+  //     },
+  //   });
+  // }
 }
