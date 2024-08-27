ddf15266eda01ac278c7ab611a0930539c88995d
ishumsk <ivan.shumski@ibm.com>
2024-08-27 20:57:17 +0300

    add swagger, implement articles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ProductModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ProductModule,
+    ArticlesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/articles/articles.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createArticleDto: CreateArticleDto) {
+    return this.prismaService.article.create({ data: createArticleDto });
+  }
+
+  findAll() {
+    return this.prismaService.article.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prismaService.article.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  findDrafts() {
+    return this.prismaService.article.findMany({
+      where: {
+        published: false,
+      },
+    });
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return this.prismaService.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prismaService.article.delete({
+      where: { id },
+    });
+  }
+}
